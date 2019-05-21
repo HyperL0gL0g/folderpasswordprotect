7a2c74b7bb65abe46f5dbb0ee66a40395e8b583e
ABHINAV GHOSH <35833124+logicinfinite@users.noreply.github.com>
2019-05-21 23:26:08 +0530

    Update password.bat

--- password.bat
+++ password.bat
@@ -18,7 +18,7 @@ goto End
 :UNLOCK
 echo Enter password to Unlock Your Secure Folder
 set/p "pass=>"
-if NOT %pass%== iamironman FAIL
+if NOT %pass%== iamironman goto FAIL
 attrib -h -s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
 ren "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" mystuffs
 echo Folder Unlocked successfully
