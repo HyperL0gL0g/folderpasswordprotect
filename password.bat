a161087205d34d98ddb2b5e566c45477bdf4301d
ABHINAV GHOSH <35833124+logicinfinite@users.noreply.github.com>
2019-05-21 23:11:16 +0530

    Update password.bat

--- password.bat
+++ password.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 if EXIST "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" goto UNLOCK
-if NOT EXIST Private goto MDPrivate
+if NOT EXIST mystuffs goto MDmystuffs
 :CONFIRM
 echo Are you sure to lock this folder? (Y/N)
 set/p "cho=>"
@@ -11,23 +11,23 @@ if %cho%==N goto END
 echo Invalid choice.
 goto CONFIRM
 :LOCK
-ren Private "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+ren mystuffs "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
 attrib +h +s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
 echo Folder locked
 goto End
 :UNLOCK
 echo Enter password to Unlock Your Secure Folder
 set/p "pass=>"
-if NOT %pass%== wonderhowtogoto FAIL
+if NOT %pass%== iamironman FAIL
 attrib -h -s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
-ren "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" Private
+ren "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" mystuffs
 echo Folder Unlocked successfully
 goto End
 :FAIL
 echo Invalid password
 goto end
-:MDPrivate
-md Private
-echo Private created successfully
+:MDmystuffs
+md mystuffs
+echo mustuffs created successfully
 goto End
 :End
